a748f8fa5cb4f14d80167519a872995c095eaab2
mcornielly <mcornielly@gmail.com>
2022-07-22 16:25:29 -0400

    template adminlte to larave and vue

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,7 +15,7 @@ mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
 
 mix.browserSync({
-    proxy: 'http://app_laravel_adminlte.test',
+    proxy: 'http://app_laravue_adminlte.test',
     // browser: 'firefox'
     browser: 'C:\\Program Files\\Firefox Developer Edition\\firefox.exe',//Firefox Developer
     // open: false
